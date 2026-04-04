b0b85f4d63fc16f20911a7cfb60d15663bf9baa4
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2026-04-04 12:58:07 +0530

    change console log

--- backend/index.js
+++ backend/index.js
@@ -51,5 +51,5 @@ app.post("/api/chat", async (req, res) => {
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server runnning on port ${PORT}`);
+  console.log(`Server runnning on port ${PORT}...`);
 });
